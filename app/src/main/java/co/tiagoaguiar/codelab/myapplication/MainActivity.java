9275076a7b1c3386f23b7434d907cc216ac36e7f
Patrick Oliveira - FWK Innovation <patrick.oliveira@fwk.global>
2022-01-24 13:55:47 -0300

    Aplicando RecyclerView na 1ª tela dos ícones

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -1,25 +1,94 @@
 package co.tiagoaguiar.codelab.myapplication;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-	private View btnImc;
+	//private View btnImc;
+	private RecyclerView rvMain;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		btnImc = findViewById(R.id.btn_imc);
+		List<MainItem> mainItens = new ArrayList<>();
+		mainItens.add(new MainItem(1,R.drawable.ic_baseline_wb_sunny_24, R.string.label_imc, R.color.colorPrimary));
+		mainItens.add(new MainItem(2,R.drawable.ic_baseline_transfer_within_a_station_24, R.string.label_tmb, ContextCompat.getColor(this, R.color.colorAccent)));
+
+		rvMain = findViewById(R.id.rv_main);
+//		rvMain.setLayoutManager(new GridLayoutManager(this,2));
+		rvMain.setLayoutManager(new LinearLayoutManager(this));
+
+		MainAdapter adapter = new MainAdapter(mainItens);
+		rvMain.setAdapter(adapter);
+
+
+		/*btnImc = findViewById(R.id.btn_imc);
 
 		btnImc.setOnClickListener(view -> {
 			Intent it = new Intent(getBaseContext(), ImcActivity.class);
 			startActivity(it);
-		});
+		});*/
+	}
+
+	private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+
+		private List<MainItem> mainItens = new ArrayList<>();
+
+		public MainAdapter(List<MainItem> mainItems) {
+			this.mainItens = mainItems;
+		}
+
+		@NonNull
+		@Override
+		public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+			return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+			MainItem mainItemCurrent = mainItens.get(position);
+			holder.bind(mainItemCurrent);
+		}
+
+		@Override
+		public int getItemCount() {
+			return mainItens.size();
+		}
+	}
+
+	//View da célula que está dentro do RecyclerView
+	private class MainViewHolder extends RecyclerView.ViewHolder {
+
+		public MainViewHolder(@NonNull View itemView) {
+			super(itemView);
+		}
+
+		public void bind(MainItem item) {
+			LinearLayout container = (LinearLayout) itemView;
+			ImageView imgIcon = itemView.findViewById(R.id.item_img_icon);
+			TextView txtName = itemView.findViewById(R.id.item_txt_name);
+
+			container.setBackgroundColor(item.getCor());
+			imgIcon.setImageResource(item.getDesenhavelId());
+			txtName.setText(item.getTextStringId());
+		}
 	}
 }
---
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainItem.java
@@ -0,0 +1,51 @@
+package co.tiagoaguiar.codelab.myapplication;
+
+public class MainItem {
+
+    private int id;
+    private int desenhavelId;
+    private int textStringId;
+    private int cor;
+
+    public MainItem(int id,
+                    int desenhavelId,
+                    int textStringId,
+                    int cor) {
+        this.id = id;
+        this.desenhavelId = desenhavelId;
+        this.textStringId = textStringId;
+        this.cor = cor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDesenhavelId() {
+        return desenhavelId;
+    }
+
+    public void setDesenhavelId(int desenhavelId) {
+        this.desenhavelId = desenhavelId;
+    }
+
+    public int getTextStringId() {
+        return textStringId;
+    }
+
+    public void setTextStringId(int textStringId) {
+        this.textStringId = textStringId;
+    }
+
+    public int getCor() {
+        return cor;
+    }
+
+    public void setCor(int cor) {
+        this.cor = cor;
+    }
+}

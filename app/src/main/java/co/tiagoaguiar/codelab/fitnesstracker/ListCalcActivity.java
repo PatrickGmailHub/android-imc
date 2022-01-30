cdab616ae61efc11b4048ca936861947352f3de3
Patrick Oliveira - FWK Innovation <patrick.oliveira@fwk.global>
2022-01-30 00:56:29 -0300

    Listando com RecyclerView os dados

--- app/src/main/java/co/tiagoaguiar/codelab/fitnesstracker/ListCalcActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/fitnesstracker/ListCalcActivity.java
@@ -1,15 +1,30 @@
 package co.tiagoaguiar.codelab.fitnesstracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import co.tiagoaguiar.codelab.myapplication.R;
 
 public class ListCalcActivity extends AppCompatActivity {
+
+    private RecyclerView rvListCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +33,72 @@ public class ListCalcActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
 
+        List<Registro> registros = new ArrayList<>();
+
         if (extras != null) {
             String type = extras.getString("type");
-            List<Registro> registros = SqlHelper.getInstance(this).getRegistroList(type);
-            Log.d("onCreate: ", registros.toString());
+            registros = SqlHelper.getInstance(this).getRegistroList(type);
+            //Log.d("onCreate: ", registros.toString());
+        }
+
+        rvListCalc = findViewById(R.id.recycler_view_list);
+        rvListCalc.setLayoutManager(new LinearLayoutManager(this));
+
+        ListCalcAdapter adapter = new ListCalcAdapter(registros);
+
+        rvListCalc.setAdapter(adapter);
+
+    }
+
+    private class ListCalcAdapter extends RecyclerView.Adapter<ListCalcViewHolder> {
+
+        List<Registro> registroList;
+
+        public ListCalcAdapter(List<Registro> registros) {
+            registroList = registros;
+        }
+
+        @NonNull
+        @Override
+        public ListCalcViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new ListCalcViewHolder(getLayoutInflater().inflate(android.R.layout.simple_list_item_1, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ListCalcViewHolder holder, int position) {
+            Registro registro = registroList.get(position);
+            holder.bind(registro);
+        }
+
+        @Override
+        public int getItemCount() {
+            return registroList.size();
+        }
+    }
+
+    private class ListCalcViewHolder extends RecyclerView.ViewHolder {
+
+        public ListCalcViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(Registro registro) {
+            Date dateSaved = null;
+            SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", new Locale("pt", "BR"));
+            try {
+                dateSaved = dtf.parse(registro.getCreatedDate());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            dtf = new SimpleDateFormat("dd/MM/yyyy", new Locale("pt", "BR"));
+            String dateFormated = dtf.format(dateSaved);
+
+
+            /* Poderia se rfeito dessa maneira pois sabemos que o itemView é um TextView
+            ((TextView) itemView).setText(registro.getType() + " - " + registro.response + " - " + registro.createdDate);
+            * */
+            TextView txtRegistro = itemView.findViewById(android.R.id.text1);
+            txtRegistro.setText(registro.getType() + ": " + registro.response + " - Data: " + dateFormated);
         }
     }
 }
--- app/src/main/java/co/tiagoaguiar/codelab/fitnesstracker/Registro.java
+++ app/src/main/java/co/tiagoaguiar/codelab/fitnesstracker/Registro.java
@@ -8,4 +8,27 @@ public class Registro {
     double response;
     String createdDate;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getResponse() {
+        return response;
+    }
+
+    public void setResponse(double response) {
+        this.response = response;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
 }
